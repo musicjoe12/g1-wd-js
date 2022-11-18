775529a05b94d52d3c1fc5800055d0aca6285c43
musicjoe12 <josephgrierson8@gmail.com>
2022-11-18 10:29:50 +0000

    exchange

---
+++ js/exchange.js
@@ -0,0 +1,49 @@
+
+
+
+(function()
+{
+    let pricePounds;
+    let exchangeRate;
+    let priceEuros;
+
+    let convertForm = document.getElementById('myForm');
+    let msg = document.getElementById('msg');
+
+    convertForm.addEventListener('submit', function(ev)
+    {
+        ev.preventDefault();
+        priceEuros = 0;
+        exchangeRate = 0.87;
+        pricePounds = parseFloat(document.getElementById('pounds').value);
+        if(isNaN(pricePounds)){
+            msg.style.display = "block";
+            msg.innerHTML = "You must enter a number";
+            msg.setAttribute('class', 'error');
+            }
+        if(pricePounds === 0){
+                msg.style.display = "block";
+                msg.innerHTML = "You must enter more than zero."
+                msg.setAttribute('class', 'error');
+        }else{
+                priceEuros = pricePounds * exchangeRate;
+                msg.style.display = "block";
+                priceEuros = priceEuros.toFixed(2);
+                msg.innerHTML = "You will get &euro;"+priceEuros;
+                msg.removeAttribute('class');
+            }
+        document.getElementById('pounds').addEventListener('focus', function()
+            {
+                this.value = "";
+                msg.innerHTML = "";
+                msg.removeAttribute('class');
+            })
+                    
+    })
+    
+    document.getElementById('pounds').value;
+
+
+
+
+})()
